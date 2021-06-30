ae54f459ed446a056a3b33ec964a97c218125884
Noel Tekiri <sicontis@gmail.com>
2021-07-01 10:49:01 +1200

    added admin cms

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ moment.locale('en');
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("./src/main.css");
+    eleventyConfig.addPassthroughCopy("./src/admin");
 
     eleventyConfig.addFilter('formatDate', date => {
         return moment(date).utc().format('LL');
